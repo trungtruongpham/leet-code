fd0f932fc05a8b15b67e84678aff796ca28e77ba
trungtruongpham <trungtruongpham@gmail.com>
2024-07-08 21:46:32 +0700

    Add LinkListCycle

---
+++ LinkListCycle/Program.cs
@@ -0,0 +1,33 @@
+﻿// Problem link: https://leetcode.com/problems/linked-list-cycle/?source=submission-noac
+
+bool HasCycle(ListNode? head)
+{
+    if(head == null) {
+        return false;
+    }
+
+    var node = head.next;
+    var hashNodes = new HashSet<ListNode>();
+
+    while(node.next != null) {
+        if(!hashNodes.Add(node)) {
+            return true;
+        }
+
+        node = node.next;
+    }
+
+    return false;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
