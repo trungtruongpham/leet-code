586249b1c5de577a1a31f5b7a18695747a5d17d4
trungtruongpham <trungtruongpham@gmail.com>
2024-07-19 14:59:44 +0700

    Add HappyNumber

---
+++ HappyNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿// Problem link: https://leetcode.com/problems/happy-number/description/?envType=study-plan-v2&envId=top-interview-150
+
+var result = IsHappy(2);
+Console.WriteLine(result);
+
+bool IsHappy(int n)
+{
+    var hashSet = new HashSet<int>();
+
+    while (hashSet.Add(n))
+    {
+        n = CalculateSquareNumbers(n);
+
+        if (n == 1)
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+int CalculateSquareNumbers(int n)
+{
+    var listNumbers = n.ToString();
+    var sumOfNumbers = 0;
+
+    foreach (var number in listNumbers)
+    {
+        sumOfNumbers += int.Parse(number.ToString()) * int.Parse(number.ToString());
+    }
+    
+    return sumOfNumbers;
+}
