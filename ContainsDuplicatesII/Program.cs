7e5677f85df9fd0fee4e07e739496bbb365071f3
trungtruongpham <trungtruongpham@gmail.com>
2024-08-05 22:04:36 +0700

    Add ContainsDuplicatesII

---
+++ ContainsDuplicatesII/Program.cs
@@ -0,0 +1,24 @@
+﻿// Problem link: https://leetcode.com/problems/contains-duplicate-ii/?envType=study-plan-v2&envId=top-interview-150
+
+var result = ContainsNearbyDuplicate([1,2,3,1], 3);
+Console.WriteLine(result);
+
+bool ContainsNearbyDuplicate(int[] nums, int k)
+{
+    var dict = new Dictionary<int, int>();
+    
+    for (var i = 0; i < nums.Length; i++)
+    {
+        if (!dict.TryAdd(nums[i], i) && Math.Abs(dict[nums[i]] - i) <= k)
+        {
+            return true;
+        }
+
+        if (dict.ContainsKey(nums[i]))
+        {
+            dict[nums[i]] = i;
+        }
+    }
+    
+    return false;
+}
